837d6d4d662b2314c700bd5cacd5f72289da16ed
Ryan Mattson <ryansmattson@gmail.com>
2016-07-04 16:51:22 -0500

    Ready to turn in.  All modes completed ecept pagination on PRO MODE.

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 $(function(){
 
-
-// $('.random-gif, .random-sticker, .search-gifs').hide();
 //Menu Items
-
 $('.label').on('click', function(event){
-  event.preventDefault();
-  $(this).parent().find('div:first').slideToggle();
 
+  event.preventDefault();
+
+  $(this).parent().find('div:first').slideToggle();
 })
 
 
 //RANDOM GIFS
-
 $('#gif-button').on('click', function(event){
+
   event.preventDefault();
-  $('.random-gif').show();
+
   var tempTag = $('#gif-tag').val();
+
   $('.gif-image').empty();
 
   $.ajax({
@@ -30,10 +29,12 @@ $('#gif-button').on('click', function(event){
 
 
 //RANDOM STICKERS
-
 $('#sticker-button').on('click', function(event){
+
   event.preventDefault();
+
   var tempTag = $('#sticker-tag').val();
+
   $('.sticker-image').empty();
 
   $.ajax({
@@ -47,51 +48,46 @@ $('#sticker-button').on('click', function(event){
 
 
 //SEARCH
-
-$('#page1').on('click', function(event){
-  event.preventDefault();
-
-
-})//click
-
 $('#search-button').on('click', function(event){
+
   event.preventDefault();
+
   var tempTag = $('#search-tag').val();
+
   $('.search-results').empty();
 
-
-  if (tempTag == ''){   //if there is no input, then return a random sticker
+  if (tempTag == ''){   //if there is no input, then dispaly a message asking for input
       $('.search-results').append('<p>Please enter a search query</p>');
-  } else {    //return a random sticker with a specific tag
+  } else {    //return gifs with inputed tag
     $.ajax({
       url: 'http://api.giphy.com/v1/gifs/search?q=' + tempTag + '&api_key=dc6zaTOxFJmzC&limit=15&offset=0',
     }).then(function(data){
       for (i = 0; i < 15; i++) {
         $('.search-results').append('<img class="img" src="' + data.data[i].images.downsized_large.url + '" alt="gif" />');
       };
-
-      // for (var i = 0; i < 25; i++) {
-      //   if(i < 4){
-      //     $('.search-results1').append('<img src="' + data.data[i].images.downsized_large.url + '" alt="gif" />');
-      //   } else if(i < 8) {
-      //     $('.search-results2').append('<img src="' + data.data[i].images.downsized_large.url + '" alt="gif" />');
-      //   } else if (i < 12) {
-      //     $('.search-results3').append('<img src="' + data.data[i].images.downsized_large.url + '" alt="gif" />');
-      //   } else if (i < 16) {
-      //     $('.search-results4').append('<img src="' + data.data[i].images.downsized_large.url + '" alt="gif" />');
-      //   } else if (i < 20) {
-      //     $('.search-results5').append('<img src="' + data.data[i].images.downsized_large.url + '" alt="gif" />');
-      //   } else if (i < 24) {
-      //     $('.search-results6').append('<img src="' + data.data[i].images.downsized_large.url + '" alt="gif" />');
-      //   } else (i < 28) {
-      //     $('.search-results7').append('<img src="' + data.data[i].images.downsized_large.url + '" alt="gif" />');
-      //   }
-      // }//for
-      // #('.search-results1').addClass('show');
     }).catch (function(){
       //error message
     })
   }//else
+})//click
+
+
+$('#trending-button').on('click', function(event){
+
+  event.preventDefault();
+
+  $('.trending-results').empty();
+
+  //return top 10 trending GIFs
+    $.ajax({
+      url: 'http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&limit=10',
+    }).then(function(data){
+      for (i = 0; i < 10; i++) {
+        $('.trending-results').append('<img class="img" src="' + data.data[i].images.downsized_large.url + '" alt="gif" />');
+      };
+    }).catch (function(){
+      //error message
+    })
 })//click
 
 
